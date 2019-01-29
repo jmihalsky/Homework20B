cceaf3aa477155c5ae8c63272e659c9279d1c5d1
jeff mihalsky <jmihalsky@gmail.com>
2019-01-28 18:41:49 -0800

    logo fix 4

--- client/src/components/jumbotronHeader/index.js
+++ client/src/components/jumbotronHeader/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 export default function Jumbotron(props) {
     return (
         <div id="JumboHeader" className="jumbotron text-center" style={{ margin: "20px" }}>
-            <h1><img src="./images/Google_Books_logo_2015.PNG" alt="Google Books React Library"/>React Library</h1>
+            <h1><img src="/images/Google_Books_logo_2015.PNG" alt="Google Books React Library"/>React Library</h1>
             {props.children}
         </div>
     )
